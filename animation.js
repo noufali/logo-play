974714309e7ed79bdbfc898c900e3ba6dcfbdc39
Nouf Aljowaysir <na1215@nyu.edu>
2018-12-13 14:38:11 -0500

    rotation

--- animation.js
+++ animation.js
@@ -1,19 +1,43 @@
-setInterval(function(){ animate(); }, 5000);
+var status = 0;
+
+setInterval(function(){
+  if (status == 0){
+    console.log('here');
+    status = 1;
+  } else {
+    console.log('nope');
+    status = 0;
+  }
+  animate();
+}, 2800);
 
 animate();
 
 function animate() {
+  console.log(status);
   var x = 100;
 
   for (let i=0;i<21;i++){
     let bounce = new Bounce();
 
-    bounce.translate({
-      from: { x: 1000, y: 10 },
-      to: { x: 0, y: 0},
-      duration: 9000-x
-    });
-
+    // bounce.translate({
+    //   from: { x: 1000, y: 10 },
+    //   to: { x: 0, y: 0},
+    //   duration: 9000-x
+    // });
+    if (status == 0){
+      bounce.rotate({
+        from: 0,
+        to: 360,
+        duration: 7000
+      });
+    } else {
+      bounce.rotate({
+        from: 360,
+        to: 0,
+        duration: 7000
+      });
+    }
     let animation = "d-animation" + i;
     bounce.define(animation);
     bounce.applyTo(document.getElementById('d' + i));
@@ -25,11 +49,25 @@ function animate() {
   for (let j=0;j<30;j++){
     let bounce = new Bounce();
 
-    bounce.translate({
-      from: { x: -1000, y: 10 },
-      to: { x: 0, y: 0},
-      duration: 10000-y
-    });
+    // bounce.translate({
+    //   from: { x: -1000, y: 10 },
+    //   to: { x: 0, y: 0},
+    //   duration: 10000-y
+    // });
+
+    if (status == 0){
+      bounce.rotate({
+        from: 0,
+        to: 360,
+        duration: 7000
+      });
+    } else {
+      bounce.rotate({
+        from: 360,
+        to: 0,
+        duration: 7000
+      });
+    }
 
     let animation = "p-animation" + j;
     bounce.define(animation);
@@ -41,11 +79,16 @@ function animate() {
   for (let a=0;a<41;a++){
     let bounce = new Bounce();
 
-    bounce.translate({
-      from: { x: -9000, y: 230 },
-      to: { x: 0, y: 0},
-      duration: 10000-z
-    });
+    // bounce.translate({
+    //   from: { x: -9000, y: 230 },
+    //   to: { x: 0, y: 0},
+    //   duration: 10000-z
+    // });
+
+    // bounce.skew({
+    //   from: { x: 0, y: 0 },
+    //   to: { x: 20, y: 0 }
+    // });
 
     let animation = "a-animation" + a;
     bounce.define(animation);
@@ -53,138 +96,3 @@ function animate() {
     z-=100;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// var c = 1060;
-// var num = 135;
-//
-// for (let i=0;i<10;i++){
-//   var img = document.createElement("img");
-//   var bounce7 = new Bounce();
-//   img.src = "img/p.png"
-//   img.id = "p";
-//   img.style.top= "440px";
-//   img.style.left = c + "px";
-//
-//   // bounce7.scale({
-//   //   from: { x: 1, y: 1},
-//   //   to: { x: 10, y: 1 },
-//   //   easing: "sway",
-//   //   duration: 300,
-//   //   delay: 30,
-//   // })
-//   //console.log(z + "px");
-//   document.body.appendChild(img);
-//   //bounce7.define("p-animation");
-//   //bounce4.applyTo(document.getElementById('p'));
-//   c+= 135;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var one,two,three,four;
-// var x = 0.001;
-// var x2 = 0.001;
-// var x3 = 0.001;
-// var x4 = 0.001;
-// function preload(){
-//   one = loadImage('img/rotate1.png');
-//   two = loadImage('img/rotate2.png');
-//   three = loadImage('img/rotate3.png');
-//   four = loadImage('img/rotate4.png');
-// }
-// function setup()
-// {
-//   createCanvas(900,900);
-// }
-//
-// function draw()
-// {
-//   background('#FAC9C0');
-//   translate(width/2,height/2);
-//   ellipse(0,0,10,10);
-//   fill('#CE2927');
-//   noStroke();
-//
-//   push();
-//   rotate(x);
-//   image(one,-300,-300,600,600);
-//   pop();
-//
-//   push();
-//   rotate(-x2);
-//   image(two,-185,-185,370,370);
-//   pop();
-//
-//   push();
-//   rotate(x3);
-//   image(three,-110,-110,220,220);
-//   pop();
-//
-//   push();
-//   rotate(-x4);
-//   image(four,-60,-60,120,120);
-//   pop();
-//
-//   x+=0.05;
-//   x2+=0.01;
-//   x3+=0.009;
-//   x4+=0.006;
-//
-//
-//   //console.log('hi');
-// }
